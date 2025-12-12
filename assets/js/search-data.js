0b3e938aed4808ee4fc7eae7392d9bc03eafac79
hardik1408 <hardikj22@iitk.ac.in>
2025-12-12 11:06:01 +0000

    Deploying to gh-pages from @ hardik1408/hardik1408.github.io@92ab93d8450b35cf6a8c4cd53e93fe90a80d9a87 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -443,7 +443,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/Hardik_Resume.pdf", "_blank");
         },
       },{
         id: 'social-email',
@@ -460,32 +460,18 @@ ninja.data = [{
           window.open("https://github.com/hardik1408", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://www.linkedin.com/in/hardik-jindal-1a2176250", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-x',
+        title: 'X',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://twitter.com/Hads1408", "_blank");
         },
       },{
       id: 'light-theme',
